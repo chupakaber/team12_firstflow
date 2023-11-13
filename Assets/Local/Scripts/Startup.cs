2088b750120b7d8777c7497c998f1bde9a179b87
Chupakaber <chupakaber@inbox.ru>
2023-11-13 18:48:46 +0300

    * add dependency to InputSyStem * add Startup.cs - global code entry point

---
+++ Assets/Local/Scripts/Startup.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Scripts
+{
+    public class Startup : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void FixedUpdate()
+        {
+
+        }
+    }
+}
