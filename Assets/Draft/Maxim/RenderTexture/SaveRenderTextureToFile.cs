d4ef4084521b41fbe6307bb3c0eb0ca1d07b37d5
Chupakaber <chupakaber@inbox.ru>
2024-02-10 11:49:39 +0300

    * fix building issue

--- Assets/Draft/Maxim/RenderTexture/SaveRenderTextureToFile.cs
+++ Assets/Draft/Maxim/RenderTexture/SaveRenderTextureToFile.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 
@@ -27,3 +28,4 @@ public class SaveRenderTextureToFile {
         return Selection.activeObject is RenderTexture;
     }
 }
+#endif
